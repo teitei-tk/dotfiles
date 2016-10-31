59a490e58ffd07b00ef9b190c2da3cc2e07d6e4e
teitei-tk <teitei.tk@gmail.com>
2016-10-31 21:27:46 +0900

    setup brew files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import os
 import subprocess
 
-EXCLUDE_LIST = [".git", "README.md", "setup.py", ".gitmodules"]
+EXCLUDE_LIST = [".git", "README.md", "setup.py", ".gitmodules", "Brewlist.txt"]
 
 
 class Installer(object):
@@ -18,7 +18,12 @@ class Installer(object):
         return os.path.dirname(os.path.abspath(__file__))
 
     def run(self):
+        print("Start cmd setup")
+
         self.dotfiles()
+        self.home_brew()
+
+        print("All Task is Successfully")
 
     def dotfiles(self):
         print("Processing dotfiles setup..")
@@ -38,6 +43,18 @@ class Installer(object):
                 os.symlink(source, dest)
 
         print("Finished Successfully")
+
+    def home_brew(self):
+        brew_file_list = [x.strip() for x in open("./Brewlist.txt").readlines()]
+
+        print("Install brew libs")
+
+        installed_list = ' '.join(brew_file_list)
+        command = "brew install {0}".format(installed_list)
+        subprocess.call(command, shell=True)
+
+        print("Finished Successfully")
+
 
 if __name__ == "__main__":
     Installer().run()
