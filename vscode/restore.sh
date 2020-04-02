5d136c7d5bc9fb1b095610e2fa66203b7766554d
teitei-tk <teitei.tk@gmail.com>
2020-04-02 19:16:15 +0900

    fix vscode settings symlink error

--- vscode/restore.sh
+++ vscode/restore.sh
@@ -3,15 +3,15 @@
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-if [ -e "$VSCODE_SETTING_DIR/settings.json" ]; then
-  rm "$VSCODE_SETTING_DIR/settings.json"
-  ln -s "$SCRIPT_DIR/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
+  rm "${VSCODE_SETTING_DIR}/settings.json"
 fi
+ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-if [ -e "$VSCODE_SETTING_DIR/keybindings.json" ]; then
-  rm "$VSCODE_SETTING_DIR/keybindings.json"
-  ln -s "$SCRIPT_DIR/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
+  rm "${VSCODE_SETTING_DIR}/keybindings.json"
 fi
+ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 cat ./extensions | while read line
 do
