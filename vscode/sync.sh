ccee5aeeda448d7174edd1dfe1a7de5294fc741c
teitei-tk <teitei.tk@gmail.com>
2021-05-13 21:19:37 +0900

    fix vscode setup

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -5,6 +5,10 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
+if [ ! -d "$VSCODE_SETTING_DIR" ]; then
+  mkdir -p "$VSCODE_SETTING_DIR"
+fi
+
 if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
   rm "${VSCODE_SETTING_DIR}/settings.json"
 fi
@@ -15,8 +19,14 @@ if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
 fi
 ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
-cat < ./extensions | while read -r line
-do
-  code --install-extension "$line"
-done
-code --list-extensions > extensions
+if [ -x "$(command -v code)" ]; then
+  cat < ./extensions | while read -r line
+
+  do
+    code --install-extension "$line"
+  done
+
+  code --list-extensions > extensions
+else
+  echo "VSCode is not installed."
+fi
