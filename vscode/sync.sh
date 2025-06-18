2e2797d06a6719989872bb33cedd4c68f3295e1b
Yoshikatsu Higa <teitei.tk@gmail.com>
2025-06-18 20:36:18 +0900

    test

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -9,6 +9,7 @@ if [ ! -d "$VSCODE_SETTING_DIR" ]; then
   mkdir -p "$VSCODE_SETTING_DIR"
 fi
 
+
 if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
   rm "${VSCODE_SETTING_DIR}/settings.json"
 fi
